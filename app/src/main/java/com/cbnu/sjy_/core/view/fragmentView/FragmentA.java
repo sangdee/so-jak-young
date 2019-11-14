1ecd13074a668226b7306fb63d7016b75b9b515f
sangji06 <tkdwl06@naver.com>
2019-11-14 21:49:37 +0900

    no message

---
+++ app/src/main/java/com/cbnu/sjy_/core/view/fragmentView/FragmentA.java
@@ -0,0 +1,18 @@
+package com.cbnu.sjy_.core.view.fragmentView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.cbnu.sjy_.R;
+
+public class FragmentA extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frament_a, container, false);
+    }
+}
